69d7b8fd5c2e5339915a647fe805f94a2094259f
mamadou6c <mohalac6@gmail.com>
2025-10-02 02:41:58 +0000

    Remove unused import and some type checking

--- services/phone_lookup.py
+++ services/phone_lookup.py
@@ -1,34 +1,43 @@
-from typing import Optional, Dict
-from phonenumbers import parse, geocoder as phone_geocoder, carrier
+from typing import Dict
+from typing import Union
+
 from opencage.geocoder import OpenCageGeocode
-from logging import Logger
-from constants.colors import GREEN, RED
+from phonenumbers.geocoder import description_for_number
+from phonenumbers.carrier import name_for_number
+from phonenumbers import parse
 
 from config import Config
 
 class PhoneLookup:
-    def __init__(self, api_key: str = None):
-        self.api_key = api_key if api_key is not None else Config.OPENCAGE_API_KEY
+    def __init__(self, api_key: Union[ str | None ]  = None) -> None:
+        self.api_key : str | None = api_key if api_key is not None else Config.OPENCAGE_API_KEY
 
-    def lookup(self, phone_number: str) -> Optional[Dict]:
+        if self.api_key is None:
+            raise ValueError("No api key found ! Make sure your api key is set in .env file or pass it as argument when initializing the PhoneLookup class.")
+
+    def lookup(self, phone_number: Union[str | None] = None) -> Union[ Dict | None]:
+
+        if phone_number is None:
+            raise ValueError(f"Phone number not provide ! Phone number is : {phone_number}")
+            
+        if not isinstance(phone_number, str):
+            raise TypeError(f"Phone number most be a string. provide <{type(phone_number)}>")
+            
         try:
             parsed_number = parse(phone_number, None)
-
-            location = phone_geocoder.description_for_number(parsed_number, "en")
-
-            service_provider = carrier.name_for_number(parsed_number, "en")
-
+            location = description_for_number(parsed_number, "en")
+            service_provider = name_for_number(parsed_number, "en")
             opencage_geocoder = OpenCageGeocode(self.api_key)
             results = opencage_geocoder.geocode(str(location))
 
             if results and len(results):
                 lat = results[0]['geometry']['lat']
                 lng = results[0]['geometry']['lng']
-                formatted_address = results[0]['formatted']
+                formatted_address : str = results[0]['formatted']
             else:
                 lat, lng, formatted_address = None, None, "Not found"
 
-            result = {
+            result : Dict = {
                 "phone": phone_number,
                 "country": location,
                 "service_provider": service_provider,
